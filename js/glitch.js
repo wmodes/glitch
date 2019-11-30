96b0d5ff117d4284ce22933ba9ec934afff8b8a0
Wes Modes <wmodes@gmail.com>
2019-11-29 21:25:35 -0800

    Update glitch.js

--- js/glitch.js
+++ js/glitch.js
@@ -3,12 +3,71 @@
 //     document.body.classList.add('glitched');
 // });
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
     $('body').addClass('glitched');
 
     $('h1,h2,h3,h4').addClass('contentTitle');
     $('p').addClass('contentText');
+
+    imgDir = 'img/';
+    overlays = [
+        'glitch-overlay-1.png',
+        'glitch-overlay-2.png',
+        'glitch-overlay-3.png',
+        'glitch-overlay-4.png',
+        'glitch-overlay-5.png',
+        'glitch-overlay-6.png',
+        'glitch-overlay-7.png',
+        'glitch-overlay-8.png',
+        'glitch-overlay-9.png'
+    ];
+
+    function addGlitchOverlays() {
+        $("body").append('<div class="overlay overlay-white">');
+        for (var i=0;i<overlays.length;i++) {
+            newElHTML = `<div class="overlay overlay${i}" ` +
+                `style="background-image:url(${imgDir + overlays[i]});">`;
+            $("body").append(newElHTML);
+        }
+    }
+
+    /**
+     * Returns a random integer between min (inclusive) and max (inclusive).
+     * The value is no lower than min (or the next integer greater than min
+     * if min isn't an integer) and no greater than max (or the next integer
+     * lower than max if max isn't an integer).
+     * Using Math.round() will give you a non-uniform distribution!
+     */
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function flashOverlays() {
+        // set delay in ms
+        delay = 500;
+        // set z-index
+        zBack = -1000;
+        zFront = 1000;
+        // select how many
+        min = 1;
+        max = 4;
+        howMany = getRandomInt(min, max);
+        // pick overlays
+        for (var i=0; i<=howMany; i++) {
+            randIndex = getRandomInt(0, overlays.length - 1);
+            console.log("flash: .overlay" + randIndex);
+            setTimeout(function(){
+                console.log("showing .overlay" + randIndex + " (z-index:"+(zFront+i) + ")");
+                $('.overlay' + randIndex).css('z-index', zFront+i)
+            }.bind(randIndex, i), delay*i);
+        }
+        // setTimeout(function(){
+        //         $('.overlay').css('z-index', zBack);
+        // }, delay*(howMany+1));
+    }
 
     function glitchImages() {
         $('img').addClass("been-glitched");
@@ -54,5 +113,6 @@ $(document).ready(function(){
     $(window).resize(function(){
         repositionGlitch();
     });
+    addGlitchOverlays();
 
-});
+// });
